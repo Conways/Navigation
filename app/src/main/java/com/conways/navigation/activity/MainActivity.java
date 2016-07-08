a46729dc93da66ea8279adb7ebf3f08973f0b31d
Conways <Conways@163.com>
2016-07-08 16:22:21 +0800

    添加BaiduMap SDK

--- app/src/main/java/com/conways/navigation/activity/MainActivity.java
+++ app/src/main/java/com/conways/navigation/activity/MainActivity.java
@@ -1,15 +1,45 @@
 package com.conways.navigation.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 import com.conways.navigation.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplication());
+
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        mapView = (MapView) findViewById(R.id.bmapView);
+        mapView.setMapCustomEnable(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
